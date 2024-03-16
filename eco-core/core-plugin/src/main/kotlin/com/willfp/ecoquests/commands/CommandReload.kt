149245b7b22c2bfa78d1567f6656e4462ffa1da8
MrPibbly <109881084+MrPibbly@users.noreply.github.com>
2024-03-15 23:24:00 -0500

    Update CommandReload.kt

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoquests/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoquests/commands/CommandReload.kt
@@ -2,6 +2,7 @@ package com.willfp.ecoquests.commands
 
 import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.ecoquests.tasks.Tasks
 import org.bukkit.command.CommandSender
 
 class CommandReload(plugin: EcoPlugin) : Subcommand(
@@ -11,6 +12,7 @@ class CommandReload(plugin: EcoPlugin) : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
+        Tasks.reload(plugin)
         plugin.reload()
         sender.sendMessage(plugin.langYml.getMessage("reloaded"))
     }
